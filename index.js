7dfe22747e092f80c74bdf6e6eaedc1016d9b42d
Varun <varunvaru524@gmail.com>
2023-03-17 00:55:07 +0530

    Testing Mongodb Atlas

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const express = require('express')
 const app = express()
 const config = require('config')
 
+app.use(express.json())
 
+
+let schema = new mongoose.Schema({
+    name:String
+})
+let Model = mongoose.model('testing',schema)
+let doc = new Model({name:'Varun'})
 
 app.get('/',(request,response)=>{
-    response.send('hellow world')
+    doc.save().then(resolve=>{
+        response.send('hello world')
+    }).catch(reject=>{
+        response.send('Something Wrong')
+    })
 })
+
 
 mongoose.connect(config.get('db'))
 .then(resolve=>console.log('Successfully Connected to Database'))
