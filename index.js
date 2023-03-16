030fcfeb003d3183dbaf8ef2c21073e06443be5b
Varun <varunvaru524@gmail.com>
2023-03-17 00:10:22 +0530

    Updated Config module

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+const config = require('config')
+
 
 
 app.get('/',(request,response)=>{
     response.send('hellow world')
 })
 
-app.listen(3000, ()=>console.log(`Listning in port 3000...`))
+mongoose.connect(config.get('db'))
+.then(resolve=>console.log('Successfully Connected to Database'))
+.catch(reject=>console.log('Failed to connect to Database'))
+
+app.listen(config.get('port'), ()=>console.log(`Listning in port 3000...`))
