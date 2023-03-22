6e2de3877025aca6a57c5fafca9d2c23d0912cf9
Varun <varunvaru524@gmail.com>
2023-03-22 18:11:52 +0530

    Added additional error handling and refactored index file

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const config = require('config')
 const helmet = require('helmet')
 const cors = require('cors')
 const compression = require('compression')
-
 const signup = require('./APIs/signup')
 const login = require('./APIs/login')
 const app = express()
 
 
+// Unexpected Error Handling
+process.on('uncaughtException',(error)=>{
+    console.log('Uncaught Exceptions',error);
+})
+process.on('unhandledRejection',(error)=>{
+    console.log('Unhandled Rejections',error);
+})
+
+
+// Middlewares and routes
 app.use(express.json())
 app.use(helmet())
 app.use(compression())
@@ -18,8 +27,10 @@ app.use('/api/signup', signup)
 app.use('/api/login',login)
 
 
+// To Database
 mongoose.connect(config.get('db'))
 .then(resolve=>console.log('Successfully Connected to Database'))
 .catch(reject=>console.log('Failed to connect to Database'))
 
-app.listen(config.get('port'), ()=>console.log(`Listning in port 3000...`))
+// To Server
+module.exports = app.listen(config.get('port'), ()=>console.log(`Listning in port 3000...`))
