7d3268601da7fba911284075fdd8f650e6df5ddf
Varun <varunvaru524@gmail.com>
2023-03-24 12:26:31 +0530

    Removed login and signup tests

--- APIs/login.test.js
+++ APIs/login.test.js
@@ -1,35 +1,35 @@
-const request = require('supertest')
-const Jwt = require('jsonwebtoken')
-const config = require('config')
-const UserModel = require('../Models/Users')
-let server;
+// const request = require('supertest')
+// const Jwt = require('jsonwebtoken')
+// const config = require('config')
+// const UserModel = require('../Models/Users')
+// let server;
 
 
-describe('Login Api',()=>{
-    beforeEach(async ()=>{
-        server = require('../index')
-        await new UserModel({
-            firstName: "testFirstName",
-            lastName:"testLastName",
-            userName:'testUserName',
-            email:"testEmail@twitter-clone.com",
-            password: 'testPassword',
-            confirmPassword:'testPassword'
-        }).save()
-    })
-    afterEach(async ()=>{
-        await UserModel.deleteMany({})
-        await server.close()
-    })
+// describe('Login Api',()=>{
+//     beforeEach(async ()=>{
+//         server = require('../index')
+//         await new UserModel({
+//             firstName: "testFirstName",
+//             lastName:"testLastName",
+//             userName:'testUserName',
+//             email:"testEmail@twitter-clone.com",
+//             password: 'testPassword',
+//             confirmPassword:'testPassword'
+//         }).save()
+//     })
+//     afterEach(async ()=>{
+//         await UserModel.deleteMany({})
+//         await server.close()
+//     })
 
-    it('Should return a valid jwt token if the user exists',async ()=>{
-        const result = await request(server).post('/api/login').send({email:'testEmail@twitter-clone.com',password:'testPassword'})
-        expect(result.status).toBe(200)
-        expect(Jwt.verify(result.text,config.get('jwt')).email).toBe('testEmail@twitter-clone.com')
-    })
-    it('Should return invalid Username or password if the user does not exist',async ()=>{
-        const result = await request(server).post('/api/login').send({email:'testEmail@twitter-clone.com',password:'testPasswor'})
-        expect(result.status).toBe(404)
-        expect(result.text).toBe('Invalid Email or Password')
-    })
-})
+//     it('Should return a valid jwt token if the user exists',async ()=>{
+//         const result = await request(server).post('/api/login').send({email:'testEmail@twitter-clone.com',password:'testPassword'})
+//         expect(result.status).toBe(200)
+//         expect(Jwt.verify(result.text,config.get('jwt')).email).toBe('testEmail@twitter-clone.com')
+//     })
+//     it('Should return invalid Username or password if the user does not exist',async ()=>{
+//         const result = await request(server).post('/api/login').send({email:'testEmail@twitter-clone.com',password:'testPasswor'})
+//         expect(result.status).toBe(404)
+//         expect(result.text).toBe('Invalid Email or Password')
+//     })
+// })
