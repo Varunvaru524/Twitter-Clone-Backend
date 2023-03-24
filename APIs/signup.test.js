2496dcc7a31309c02e8c955b103a9773d44fe9ff
Varun <varunvaru524@gmail.com>
2023-03-24 12:46:46 +0530

    Fixed all test cases and all tests are passing

--- APIs/signup.test.js
+++ APIs/signup.test.js
@@ -1,48 +1,53 @@
-// const request = require('supertest')
-// const Jwt = require('jsonwebtoken')
-// const config = require('config')
-// const UserModel = require('../Models/Users')
-// let server;
+const mongoose = require('mongoose')
+const request = require('supertest')
+const Jwt = require('jsonwebtoken')
+const config = require('config')
+const UserModel = require('../Models/Users')
+let server;
 
 
-// describe('Signup Api',()=>{
-//     beforeEach(()=>{server = require('../index')})
-//     afterEach(async ()=>{
-//         await UserModel.deleteMany({})
-//         await server.close()
-//     })
+describe('Signup Api',()=>{
+    beforeEach(async ()=>{
+        await mongoose.connect(config.get('db'))
+        server = require('../index')
+    })
+    afterEach(async ()=>{
+        await UserModel.deleteMany({})
+        await mongoose.disconnect()
+        await server.close()
+    })
 
-//     it('Should return a valid jwt token tn Successfull registration',async ()=>{
-//         const result = await request(server).post('/api/signup').send({
-//             firstName: "testFirstName",
-//             lastName:"testLastName",
-//             userName:'testUserName',
-//             email:"testEmail@twitter-clone.com",
-//             password: 'testPassword',
-//             confirmPassword:'testPassword'
-//         })
-//         expect(result.status).toBe(200)
-//         expect(Jwt.verify(result.text,config.get('jwt')).email).toBe('testEmail@twitter-clone.com')
-//     })
-//     it('Should return User already registered',async ()=>{
-//         await new UserModel({
-//             firstName: "testFirstName",
-//             lastName:"testLastName",
-//             userName:'testUserName',
-//             email:"testEmail@twitter-clone.com",
-//             password: 'testPassword',
-//             confirmPassword:'testPassword'
-//         }).save()
+    it('Should return a valid jwt token tn Successfull registration',async ()=>{
+        const result = await request(server).post('/api/signup').send({
+            firstName: "testFirstName",
+            lastName:"testLastName",
+            userName:'testUserName',
+            email:"testEmail@twitter-clone.com",
+            password: 'testPassword',
+            confirmPassword:'testPassword'
+        })
+        expect(result.status).toBe(200)
+        expect(Jwt.verify(result.text,config.get('jwt')).email).toBe('testEmail@twitter-clone.com')
+    })
+    it('Should return User already registered',async ()=>{
+        await new UserModel({
+            firstName: "testFirstName",
+            lastName:"testLastName",
+            userName:'testUserName',
+            email:"testEmail@twitter-clone.com",
+            password: 'testPassword',
+            confirmPassword:'testPassword'
+        }).save()
 
-//         const result = await request(server).post('/api/signup').send({
-//             firstName: "testFirstName",
-//             lastName:"testLastName",
-//             userName:'testUserName',
-//             email:"testEmail@twitter-clone.com",
-//             password: 'testPassword',
-//             confirmPassword:'testPassword'
-//         })
-//         expect(result.status).toBe(400)
-//         expect(result.text).toBe('User already registered')
-//     })
-// })
+        const result = await request(server).post('/api/signup').send({
+            firstName: "testFirstName",
+            lastName:"testLastName",
+            userName:'testUserName',
+            email:"testEmail@twitter-clone.com",
+            password: 'testPassword',
+            confirmPassword:'testPassword'
+        })
+        expect(result.status).toBe(400)
+        expect(result.text).toBe('User already registered')
+    })
+})
